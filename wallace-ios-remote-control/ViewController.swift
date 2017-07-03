94255185308781ce3f7bc01499dffb429b333e6d
Jonathan Böcker <jonathan.e.m.bocker@gmail.com>
2017-07-04 01:00:36 +0200

    Initial Commit

---
+++ wallace-ios-remote-control/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  wallace-ios-remote-control
+//
+//  Created by Jonathan Böcker on 2017-07-04.
+//  Copyright © 2017 Jonathan Böcker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
