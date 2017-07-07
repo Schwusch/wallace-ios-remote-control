ffc1efc3d00492fa9ef43cf5a33eb8341f23ed73
Jonathan Böcker <jonathan.e.m.bocker@gmail.com>
2017-07-07 03:27:13 +0200

    websockets babysteps

---
+++ Podfile
@@ -0,0 +1,10 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '9.0'
+
+target 'wallace-ios-remote-control' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for wallace-ios-remote-control
+  pod 'Starscream', '~> 2.0.3'
+end
--- wallace-ios-remote-control/ViewController.swift
+++ wallace-ios-remote-control/ViewController.swift
@@ -7,27 +7,23 @@
 //
 
 import UIKit
+import Starscream
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var connectingIndicator: UIActivityIndicatorView!
     @IBOutlet weak var ConnectToIPButton: UIButton!
     @IBOutlet weak var iPadressField: UITextField!
     
     @IBOutlet weak var rightMotorSpeedSlider: UISlider!{
         didSet{
-            rightMotorSpeedSlider.transform = CGAffineTransform(rotationAngle: -CGFloat.pi/2)
-            rightMotorSpeedSlider.minimumValue = -100
-            rightMotorSpeedSlider.maximumValue = 100
-            rightMotorSpeedSlider.setValue(0, animated: false)
+            initSlider(slider: rightMotorSpeedSlider)
         }
     }
     
     @IBOutlet weak var leftMotorSpeedSlider: UISlider!{
         didSet{
-            leftMotorSpeedSlider.transform = CGAffineTransform(rotationAngle: -CGFloat.pi/2)
-            leftMotorSpeedSlider.minimumValue = -100
-            leftMotorSpeedSlider.maximumValue = 100
-            leftMotorSpeedSlider.setValue(0, animated: false)
+            initSlider(slider: leftMotorSpeedSlider)
         }
     }
     
@@ -35,6 +31,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var rightSpeedSliderValue: UILabel!
     
     @IBOutlet weak var stopMotorsButton: UIButton!
+    
+    var socket: WebSocket?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -48,10 +46,12 @@ class ViewController: UIViewController {
     
     @IBAction func rightSliderValueChanged(_ sender: UISlider) {
         rightSpeedSliderValue.text = String(Int(sender.value))
+        sendMotorSpeedsToRobot(rightMotor: Int(sender.value), leftMotor: Int(leftMotorSpeedSlider.value))
     }
     
     @IBAction func leftSliderValueChanged(_ sender: UISlider) {
         leftSpeedSliderValue.text = String(Int(sender.value))
+        sendMotorSpeedsToRobot(rightMotor: Int(rightMotorSpeedSlider.value), leftMotor: Int(sender.value))
     }
     
     @IBAction func stopMotorsButtonPressed() {
@@ -60,10 +60,70 @@ class ViewController: UIViewController {
         
         rightMotorSpeedSlider.value = 0
         rightSpeedSliderValue.text = "0"
+        sendMotorSpeedsToRobot(rightMotor: 0, leftMotor: 0)
     }
     
     @IBAction func onConnectButtonPressed() {
-        print(iPadressField.text as Any)
+        
+        if validateIpAddress(ipToValidate: iPadressField.text!) {
+            socket?.disconnect()
+            
+            connectingIndicator.startAnimating()
+            ConnectToIPButton.isHidden = true
+            
+            socket = WebSocket(url: URL(string: "ws://\(iPadressField.text!):8887/")!)
+            
+            socket?.onConnect = {
+                self.connectingIndicator.stopAnimating()
+                self.ConnectToIPButton.isHidden = false
+                self.ConnectToIPButton.isEnabled = false
+                print("websocket is connected")
+            }
+            
+            socket?.onDisconnect = { (error: NSError?) in
+                self.connectingIndicator.stopAnimating()
+                self.ConnectToIPButton.isHidden = false
+                self.ConnectToIPButton.isEnabled = true
+                print("websocket is disconnected: \(error?.localizedDescription ?? "no error")")
+            }
+            
+            socket?.onText = { (text: String) in
+                print("got some text: \(text)")
+            }
+            
+            socket?.onData = { (data: Data) in
+                print("got some data: \(data.count)")
+            }
+            
+            socket?.connect()
+        } else {
+            print("Not a valid IP Address!")
+        }
+    }
+    
+    func sendMotorSpeedsToRobot(rightMotor: Int, leftMotor: Int) {
+        if (socket?.isConnected) ?? false {
+            let message = "motor: left \(leftMotor) right \(rightMotor)"
+            print("SENDING: \(message)")
+            socket?.write(string: message)
+        }
+    }
+    
+    func initSlider(slider: UISlider) {
+        slider.transform = CGAffineTransform(rotationAngle: -CGFloat.pi/2)
+        slider.minimumValue = -100
+        slider.maximumValue = 100
+        slider.setValue(0, animated: false)
+    }
+    
+    func validateIpAddress(ipToValidate: String) -> Bool {
+        var sin = sockaddr_in()
+        
+        if ipToValidate.withCString({ cstring in inet_pton(AF_INET, cstring, &sin.sin_addr) }) == 1 {
+            return true
+        }
+        
+        return false
     }
 }
 
